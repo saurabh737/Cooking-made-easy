fafd93d2a03d46a10cc7eb0244550c573f9f7159
Kumar Saurabh <kumarsaurabh737@gmail.com>
2022-04-23 18:13:42 -0400

    Log out feature

--- webapp/website/__init__.py
+++ webapp/website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_session import Session
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,6 +11,9 @@ def create_app():
     app.config['SECRET_KEY'] = 'dexlab'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    app.config["SESSION_PERMANENT"] = False
+    app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
     
     from .views import views
     from .auth import auth
